df64a49e4d53e3f3724a1a000aaea4bb60273cad
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 03:49:33 -0700

    main.js test fix

--- personalpage/src/main.js
+++ personalpage/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './css/main.css';
-import HobbyInterest from './components/hobint.vue';
+
 
 const app = createApp(App)
-app.component('hobby-interest', HobbyInterest)
+
 app.mount('#app')
