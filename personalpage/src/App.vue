bfa7afa17a0f72e9a0e9fb346579908af5a70dce
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 03:10:48 -0700

    supabase test

--- personalpage/src/App.vue
+++ personalpage/src/App.vue
@@ -56,7 +56,9 @@
             <img src="./images/radiologo.png"></img>
           </div> 
         </div> 
-
+          <div>  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul></div>
 
       </div>
 
@@ -68,7 +70,27 @@
 </template>
 
 <script setup>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
 
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
 </script>
 
 <style scoped>
