8973aac58b5d613c36af5ac43b497a50128539fa
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 07:52:46 -0700

    supabase fix 00

--- personalpage/src/App.vue
+++ personalpage/src/App.vue
@@ -35,11 +35,25 @@
         <guestlog/>
       </div>
     </footer>
+    <div>
+      <ul>
+       <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+      </ul>
+    </div>
 
   </div>
 </template>
 
 
 <script setup>
-
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+const instruments = ref([])
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+onMounted(() => {
+   getInstruments()
+})
 </script>
