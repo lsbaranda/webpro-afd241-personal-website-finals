621c8ab6bebcf5528d149e035543deea2369e37c
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 08:03:08 -0700

    supabase path fix 03

--- personalpage/src/components/guestlog.vue
+++ personalpage/src/components/guestlog.vue
@@ -13,7 +13,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { supabase } from '../supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 
 const guestbooks = ref([])
