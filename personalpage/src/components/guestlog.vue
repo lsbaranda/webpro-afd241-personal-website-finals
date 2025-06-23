268bfc60ef09f5fe83eb88ad0daa6a7d71194e47
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 20:44:43 -0700

    uestlog format fix 01

--- personalpage/src/components/guestlog.vue
+++ personalpage/src/components/guestlog.vue
@@ -6,7 +6,7 @@
     
     <ul>
       <li v-for="guestbooks in guestbooks" :key="guestbooks.id">
-        <div class="flex-con type4"></div>
+        <div class="flex-con type4">
         <p> Visitor:  {{ guestbooks.name }}</p> <p> Reason for visiting:  {{ guestbooks.reason }}</p>
         </div>
       </li>
