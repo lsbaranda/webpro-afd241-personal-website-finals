0d86499e51379d27ef76c6246ddfd8bed590062f
lsbaranda <lsbaranda@student.apc.edu.ph>
2025-06-22 21:39:39 -0700

    thanks reformat again

--- personalpage/src/App.vue
+++ personalpage/src/App.vue
@@ -49,9 +49,9 @@
     </div>
     <div class="flex-con type2">
       <div class="flex-con type4">
-      <span class="title"> Speacial thanks: </span><br></br>
-      <span class="contents">Mr. Joe Quesada</span><br></br>
-      <span class="contents">Ms. Sharmaine Migriño</span><br></br>
+      <span class="title"> Speacial thanks: </span>
+      <span class="contents">Mr. Joe Quesada</span>
+      <span class="contents">Ms. Sharmaine Migriño</span>
       <span class="contents">and the rest of AFD241 - WEBPROG Class</span>
       </div>
     </div>
